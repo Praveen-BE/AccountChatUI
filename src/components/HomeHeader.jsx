c2df4780297e8bbec11775705f8d5a121149364c
Praveen-BE <praveenrajendiran3939@gmail.com>
2025-06-22 11:17:35 +0530

    more animation

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -5,35 +5,48 @@ import {
   TableCellsIcon,
 } from "@heroicons/react/24/solid";
 import { FunnelIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router";
 
 const HomeHeader = () => {
-  const [isShowMore, setIsShowMore] = useState(true);
+  const divRef = useRef(null);
+  const [isShowMore, setIsShowMore] = useState(false);
+  const toggleShowMore = () => {
+    setIsShowMore(!isShowMore);
+    triggerAnimation();
+  };
 
+  const triggerAnimation = () => {
+    const el = divRef.current;
+    el.classList.remove("more-active");
+
+    // Trigger reflow to reset the animation
+    void el.offsetWidth;
+
+    el.classList.add("more-active");
+  };
   return (
     <div className="home-header">
-      <button
-        onClick={() => setIsShowMore(!isShowMore)}
-        className="home-header__btn"
-      >
+      <button onClick={() => toggleShowMore()} className="home-header__btn">
         <EllipsisVerticalIcon />
-        {isShowMore && (
-          <div className="more-active">
-            <Link to={"/creategroup"}>
-              <p>Create Group</p>
-            </Link>
-            <Link to={"/adddevice"}>
-              <p>Linked Devices</p>
-            </Link>
-            <Link to={"/settings"}>
-              <p>Starred Messages</p>
-            </Link>
-            <Link to={"/starred"}>
-              <p>Settings</p>
-            </Link>
-          </div>
-        )}
+
+        <div
+          ref={divRef}
+          className={"more-active " + (isShowMore ? " " : "hide-more-active")}
+        >
+          <Link to={"/creategroup"}>
+            <p>Create Group</p>
+          </Link>
+          <Link to={"/adddevice"}>
+            <p>Linked Devices</p>
+          </Link>
+          <Link to={"/settings"}>
+            <p>Starred Messages</p>
+          </Link>
+          <Link to={"/starred"}>
+            <p>Settings</p>
+          </Link>
+        </div>
       </button>
       <button className="home-header__logo">
         <PaperAirplaneIcon className="paperAirplaneIcon" />
